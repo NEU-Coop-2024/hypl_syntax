5051a43ba5daaad457942f8bd7a2937327a663ca
Kevin Yang <kevingyang72@gmail.com>
2024-10-01 12:22:24 -0400

    num type

--- src/highlight.ts
+++ src/highlight.ts
@@ -4,14 +4,14 @@ import { HighlightStyle } from "@codemirror/language";
 export const hyplHighlight = styleTags({
   Identifier: t.variableName,
   Name: t.name,
-  Number : t.number,
-  "( )": t.paren
+  "( )": t.paren,
+  NewExpression: t.modifier
 });
 
 export const hyplHighlightStyle = HighlightStyle.define([
   { tag: t.variableName, color: "#2689C7" },
   { tag: t.name, color: "#d90cfe" },
-  { tag: t.number, color: "#91041e" },
+  { tag: t.modifier, color: "#91041e" },
 ]);
 
 export const hyplHighlightExtension = [hyplHighlight, hyplHighlightStyle];
